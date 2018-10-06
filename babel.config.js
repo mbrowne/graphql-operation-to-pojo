8a3b84de0677475f54ef517ad2b298c08f522f09
Matthew Browne <mbrowne83@gmail.com>
2018-10-05 21:24:30 -0400

    added class-properties to babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 const presets = ['@babel/preset-flow']
 
 const plugins = [
+    '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-transform-modules-commonjs'
 ]
